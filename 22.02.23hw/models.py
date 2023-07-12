7c61d5aa2697b386a9b3100531bb086e754d4142
Your Name <112748025+lenashalygina@users.noreply.github.com>
2023-07-12 18:23:48 +0600

    updated

--- 22.02.23hw/models.py
+++ 22.02.23hw/models.py
@@ -1,19 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
+    likes = models.ManyToManyField(User, related_name='liked_posts')
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+# views.py
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post, Comment
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'posts/post_create.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('posts:post_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'posts/post_update.html'
+    fields = ['title', 'content']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'posts/post_delete.html'
+    success_url = reverse_lazy('posts:post_list')
+
+class PostLikeView(DetailView):
+    model = Post
+
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        post.likes.add(request.user)
+        return redirect('posts:post_detail', pk=post.pk)
+
+class CommentCreateView(CreateView):
+    model = Comment
+    template_name = 'posts/comment_create.html'
+    fields = ['content']
+    success_url = reverse_lazy('posts:post_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    template_name = 'posts/comment_update.html'
+    fields = ['content']
+    success_url = reverse_lazy('posts:post_list')
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    template_name = 'posts/comment_delete.html'
+    success_url = reverse_lazy('posts:post_list')
--- 22.02.23hw/urls.py
+++ 22.02.23hw/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import PostListView
+from . import views
+
+app_name = 'posts'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('create/', views.PostCreateView.as_view(), name='post_create'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('<int:pk>/like/', views.PostLikeView.as_view(), name='post_like'),
+    path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
